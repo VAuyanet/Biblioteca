514570fb6f7b99cc1c8a303859763664c6817de2
VAuyanet <victor.auyanet@gmail.com>
2018-10-24 20:08:57 +0200

    DTO soci

--- Project/Biblioteca/Controller/ControllerSoci.cs
+++ Project/Biblioteca/Controller/ControllerSoci.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using View;
 
 namespace Controller
 {
     public class ControllerSoci
     {
-
+        Form1 f1 = new Form1();
+        BibliotecaEntities1 db = new BibliotecaEntities1();
     }
 }
---
+++ Project/Biblioteca/Model/SociDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class SociDTO
+    {
+        public int Id { get; set; }
+        public string nom { get; set; }
+        public string cognom { get; set; }
+        public DateTime dataModificacio { get; set; }
+        public DateTime dataIntroduccio { get; set; }
+        public Nullable<System.DateTime> dataBaixa { get; set; }
+    }
+}
